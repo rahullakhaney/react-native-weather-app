d24e0f5ed09c1c7e230da1d03f4e18c7fb9d0080
rahullakhaney <rahul@codebrew.io>
2017-02-09 17:09:43 +0400

    Added splash screen and const for dynamic data population

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,8 +1,13 @@
 const rootUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=4b31e7e1ee964469fbc4a1c00f1ffd2e'
 
-export const fetchWeather = (lat, lon) => {
-  const url = rootUrl+'&lat='+lat+'&lon='+lon
+export const fetchWeather = (lat,lon) => {
+  const url = rootUrl+'&lat='+lat+"&lon="+lon+"&units=metric"
   console.log(url)
 
-  fetch(url).then(res = console.log(res))
+  return fetch(url)
+    .then(res => res.json())
+    .then(json => ({
+      temp: json.main.temp,
+      weather: json.weather[0].main
+    }))
 }
